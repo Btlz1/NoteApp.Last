f5278b03f242042e0105974a01b406cf039fb9f4
Btlz1 <bealtes0@gmail.com>
2024-12-10 16:38:55 +0500

    удалил лишний метод из notesRepository

--- Prod1/obj/Debug/net8.0/Prod1.AssemblyInfo.cs
+++ Prod1/obj/Debug/net8.0/Prod1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prod1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd8d05b6a348a800278145d387113d8a80cc06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27cd502d76517b54fab2e3efba39f740b4bfddb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prod1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prod1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
